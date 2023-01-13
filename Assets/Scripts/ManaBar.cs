41dfa8a2b1d8ab5f4b13f9be4749e477d5b96846
BbIXOD <108470719+BbIXOD@users.noreply.github.com>
2023-01-13 18:13:39 +0200

    Small interface update
    Now we have canvas where we can place interface.
    Place manabar on it

--- Assets/Scripts/ManaBar.cs
+++ Assets/Scripts/ManaBar.cs
@@ -1,28 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManaBar : MonoBehaviour
 {
     private float _mana;
-    private const float PosX = -10;
     private int _type;
-    private SpriteRenderer _re;
+    private RectTransform _rt;
+    private Image _image;
     private Weapon _scr;
 
     private void Start()
     {
         _scr = GameObject.Find("Weapon").GetComponent<Weapon>();
-        _re = GetComponent<SpriteRenderer>();
+        _rt = GetComponent<RectTransform>();
+        _image = GetComponent<Image>();
     }
     
     private void Update()
     {
-        var t = transform;
-        var pos = t.position;
         _mana = _scr.mana;
         _type = _scr.BulletType;
-        t.localScale = new Vector3(_mana, 1, 1);
-        t.position = new Vector3(PosX, pos.y, pos.z);
-        _re.color = new Color(_type % 3 % 2, (_type - 1) % 2, (_type % 3 + 1) % 3 % 2, 1);
+        var r = _rt.transform;
+        var transformLocalScale = r.localScale;
+        transformLocalScale.x = _mana / 5;
+        r.localScale = transformLocalScale;
+        _image.color = new Color(_type % 3 % 2, (_type - 1) % 2, (_type % 3 + 1) % 3 % 2, 1);
         
     }
 }
