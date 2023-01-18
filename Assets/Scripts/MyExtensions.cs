6012c53f3b66ba412b18e8c4e1c6b37be21fbfdc
BbIXOD <108470719+BbIXOD@users.noreply.github.com>
2023-01-18 15:07:40 +0200

    Intarface update
    Now You can see description of object just looking at it. It allows show enemy's live, "bullets on it".
    Also downloaded TextMesh, so this commit might be big.

--- Assets/Scripts/MyExtensions.cs
+++ Assets/Scripts/MyExtensions.cs
@@ -1,5 +1,18 @@
+using UnityEngine;
 
-public static class MyExtensions
+namespace MyExtensions
 {
-    //in future i will add some methods her
+    public static class MyExtensions
+    {
+        public static Color SetAlpha(this Color col, int alpha)
+        {
+            return alpha == 1 ? SetAlpha(col, (float)alpha) :
+                new Color(col.r, col.g, col.b, (float)alpha / 255);
+        }
+        
+        public static Color SetAlpha(this Color col, float alpha)
+        {
+            return new Color(col.r, col.g, col.b, alpha);
+        }
+    }
 }
