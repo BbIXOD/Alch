a0813a193faeaef4d8837df0ea4392ad065c194a
BbIXOD <108470719+BbIXOD@users.noreply.github.com>
2023-02-03 16:56:10 +0200

    Improved compas

--- Assets/Scripts/UI/Compas.cs
+++ Assets/Scripts/UI/Compas.cs
@@ -2,25 +2,41 @@ using UnityEngine;
 
 public class Compas : MonoBehaviour
 {
-    private Enemy _spect;
+    private GameObject _spect;
+    private SpriteRenderer _sr;
+
+    private void Awake()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+        _sr.color = Color.red;
+    }
+
     private void Update()
     {
-        if (! _spect || _spect.live <= 0) Nearest();
+        if (!_spect) Nearest();
         var transform1 = transform;
         transform.rotation =
-            Quaternion.Lerp(transform1.rotation, MyExtensions.MyExtensions.Spectate(transform1.position, _spect.myPos), 2);
+            Quaternion.Lerp(transform1.rotation, MyExtensions.MyExtensions.Spectate(transform1.position, _spect.transform.position), 2);
+        
     }
 
     private void Nearest()
     {
+        var all = FindObjectsOfType<Enemy>();
+        if (all.Length == 0)
+        {
+            _sr.color = Color.green;
+            _spect = GameObject.Find("Boat");
+            if (!_spect) _spect = GameObject.Find("Purple Portal");
+        }
         var tp = transform.position;
         var magnitude = Mathf.Infinity;
-        foreach (var enemy in FindObjectsOfType<Enemy>())
+        foreach (var enemy in all)
         {
             var current = ((Vector2)(tp - enemy.myPos)).magnitude;
             if (current > magnitude) continue;
             magnitude = current;
-            _spect = enemy;
+            _spect = enemy.gameObject;
         }
     }
 }
