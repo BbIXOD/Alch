9be8a350151202bcf54d0890c72570a3312a4371
BbIXOD <108470719+BbIXOD@users.noreply.github.com>
2023-02-06 18:57:12 +0200

    Fixed Shems bugs

--- Assets/Scripts/EasterEggs/SoundPlaySecondScene.cs
+++ Assets/Scripts/EasterEggs/SoundPlaySecondScene.cs
@@ -8,11 +8,13 @@ public class SoundPlaySecondScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!collision.CompareTag("Player")) return;
         enter.Play();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (!collision.CompareTag("Player")) return;
         exit.Play();
     }
 }
