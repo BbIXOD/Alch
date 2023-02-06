fa9058f74da96f427642fe7bdc9a02387d3f596b
BbIXOD <108470719+BbIXOD@users.noreply.github.com>
2023-02-06 16:59:44 +0200

    Fixed my last change

--- Assets/Scripts/DialogSystem/DialogueManager.cs
+++ Assets/Scripts/DialogSystem/DialogueManager.cs
@@ -10,37 +10,42 @@ public class DialogueManager : MonoBehaviour
 
     public Animator boxAnim;
     public Animator startAnim;
-
-    private Queue<string> sentences;
-    private byte _index;
-
-    private void Start()
-    {
-        sentences = new Queue<string>();
-    }
+    
+    private short _index;
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Dialogue");
         boxAnim.SetBool("boxOpen", true);
         startAnim.SetBool("startOpen", false);
 
         nameText.text = dialogueText.name;
-        sentences.Clear();
-
+        _index = -1;
         
         DisplayNextSentences(dialogue.sentences);
     }
 
-    public void DisplayNextSentences(string[] sentences)
+    private void DisplayNextSentences(string[] sentences)
     {
-        if(_index == this.sentences.Count)
+        _index++;
+        if(_index == sentences.Length)
         {
             EndDialogue();
             return;
         }
-        string sentence = sentences[_index];
-        _index++;
+        var sentence = sentences[_index];
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    
+    private void DisplayPreviousSentences(string[] sentences)
+    {
+        _index--;
+        if(_index < 0)
+        {
+            EndDialogue();
+            return;
+        }
+        var sentence = sentences[_index];
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
     }
